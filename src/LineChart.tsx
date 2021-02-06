9192e09a328681866e15d239897b80397372ef87
saurabh2590@gmail.com <saurabh2590@gmail.com>
2021-02-06 17:44:40 +0100

    wip: continue drawing the chart

--- src/LineChart.tsx
+++ src/LineChart.tsx
@@ -1,30 +1,28 @@
 import React from "react";
-import {CartesianGrid, Line, LineChart, Tooltip, XAxis} from "recharts";
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 
 type LineChartProps = {}
 export const LineChartDemo: React.FC<LineChartProps> = () => {
   const data = [
-    {name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20]},
-    {name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40]},
-    {name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40},
-    {name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20},
-    {name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28},
-    {name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20]},
-    {name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40]},
-    {name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28},
-    {name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28},
-    {name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60]},
+    {month: '9', value: 1000, mustSell: 700, mustBuy: 260, valueError: [75, 20]},
+    {month: '10', value: 300, mustSell: 700, mustBuy: 260, valueError: [90, 40]},
+    {month: '11', value: 280, mustSell: 700, mustBuy: 260, valueError: 40},
+    {month: '12', value: 200, mustSell: 700, mustBuy: 260, valueError: 20},
+    {month: '1', value: 278, mustSell: 700, mustBuy: 260, valueError: 28},
+    {month: '2', value: 189, mustSell: 700, mustBuy: 260, valueError: [90, 20]},
+    {month: '3', value: 189, mustSell: 700, mustBuy: 260, valueError: [28, 40]},
+    {month: '4', value: 189, mustSell: 700, mustBuy: 260, valueError: 28},
+    {month: '5', value: 189, mustSell: 700, mustBuy: 260, valueError: 28},
   ]
-  return <LineChart
-    width={400}
-    height={400}
-    data={data}
-    margin={{top: 5, right: 20, left: 10, bottom: 5}}
-  >
-    <XAxis dataKey="name"/>
-    <Tooltip/>
-    <CartesianGrid stroke="#f5f5f5"/>
-    <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0}/>
-    <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1}/>
+  return <LineChart width={730} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="month" />
+    <YAxis orientation="right"/>
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+    <Line type="monotone" dataKey="mustBuy" stroke="#82ca9d"/>
+    <Line type="monotone" dataKey="mustSell" stroke="#82ca9d"/>
   </LineChart>
 }
